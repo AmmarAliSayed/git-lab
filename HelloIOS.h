7a734f6800bc29534abb49f63be61274ea7abe09
youssef <youssef.mohamed7557@gmail.com>
2021-02-27 14:48:33 +0200

    HelloIOS.H

---
+++ HelloIOS.h
@@ -0,0 +1,19 @@
+//
+//  class1.h
+//  macplay
+//
+//  Created by Hala on 26/02/2021.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface class1 : NSObject
+
+
+-(void) printHello1;
+
+@end
+
+NS_ASSUME_NONNULL_END
